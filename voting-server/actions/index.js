44d78f7aded4d46a391b308feecb71c26b956264
Herbert V <herbert.videla@tektonlabs.com>
2016-11-25 18:26:57 -0500

    Added an action creator that adds two movies to a new key, pair. Also this movies shouldn't be in the entries List.

--- voting-server/actions/index.js
+++ voting-server/actions/index.js
@@ -3,3 +3,12 @@ var Immutable = requiere('immutable');
 export function setEntries(state, entries) {
   return state.set('entries', Inmmutable.List(entries));
 }
+
+// Vote
+export function next(state) {
+  const entries = state.get('entries');
+  return state.merge({
+    vote: Inmmutable.Map({pair: entries.take(2)}),
+    entries: entries.skip(2)
+  });
+}
