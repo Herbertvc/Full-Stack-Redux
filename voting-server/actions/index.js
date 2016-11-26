c23275812bf0f957deacd46f0007b5d20bbb2a6f
Herbert V <herbert.videla@tektonlabs.com>
2016-11-25 19:00:59 -0500

    Modified the action creator next to load the next pair which will be voted and to be able to finish the voting process.

--- voting-server/actions/index.js
+++ voting-server/actions/index.js
@@ -4,13 +4,32 @@ export function setEntries(state, entries) {
   return state.set('entries', Inmmutable.List(entries));
 }
 
-// Vote
+function getWinners(vote) {
+  if (!vote) return [];
+  var pair = vote.get('pair');
+  var a = pair['a'];
+  var b = pair['b'];
+  var aVotes = vote.getIn(['tally', a], 0);
+  var bVotes = vote.getIn(['tally', b], 0);
+
+  if (aVotes > bVotes) return [a];
+  else if (aVotes < bVotes) return [b];
+  else return [a,b];
+}
+
 export function next(state) {
-  const entries = state.get('entries');
-  return state.merge({
-    vote: Inmmutable.Map({pair: entries.take(2)}),
-    entries: entries.skip(2)
-  });
+  var entries = state.get('entries').concat(getWinners(state.get('vote')));
+
+  if(entries.size == 1) {
+    return state.remove('vote')
+                .remove('entriies')
+                .set('winner', entries.first());
+  } else {
+    return state.merge({
+      vote: Inmmutable.Map({pair: entries.take(2)}),
+      entries: entries.skip(2)
+    });
+  }
 }
 
 // Go to the path ['vote', 'tally', entry], if the key is missing create a new map this place; if the value at the end is missing initialize with 0.
