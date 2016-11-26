142cfb153dd61a60640b11024b86b0e58f6d2942
Herbert V <herbert.videla@tektonlabs.com>
2016-11-25 19:48:35 -0500

    Added logic to the reducer

--- voting-server/reducer.js
+++ voting-server/reducer.js
@@ -4,5 +4,14 @@ var next = actions.next;
 var vote = actions.vote;
 
 export function reducer(state, action) {
-  
+  switch(action.type) {
+    case 'SET_ENTRIES':
+      return setEntries(state, action.entries);
+    case 'NEXT':
+      return next(state);
+    case 'VOTE':
+      return vote(state, action.entry);
+  }
+
+  return state;
 }
